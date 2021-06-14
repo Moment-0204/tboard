fe7a1cf4a7b6551f822126f53e02af04cb3451c6
TakumiNishino <nistak04@yahoo.co.jp>
2021-06-14 16:01:01 +0900

    コメントつけました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,25 +1,23 @@
-$visitTime=0
+# frozen_string_literal: true
+
+$visitTime = 0
 
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
-#    render plain: "railsはクソ！w"
-  end
-  
-  def sub
-  end
+  def index; end
+
+  def sub; end
 
   def reset
-    $visitTime=0
-    redirect_to "/"
+    $visitTime = 0
+    redirect_to '/'
   end
 
   def sub1
     $visitTime += 1
     p $visitTime
   end
-
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,11 +19,17 @@ class ReviewsController < ApplicationController
   end
 
   def exc_delete
+    # 削除するコメントを指定せずボタンを押したときはparams[:review]がnilで返ってくる
     unless params[:review].nil?
+      # delete_id->削除するコメントのid(本体)
+      # delete_innerid->削除するコメントのinnerid(科目・教員・コメントがセットでわかる)
+      # innerid->abbccのかたちでa:科目,b:教員,c:コメントになっている
       delete_id = params[:review][:id]
       delete_innerid = Review.find(delete_id).innerid
       delete_title = delete_innerid / 100
       reviews = Review.order(:innerid).all
+      # inneridの下２桁が00のときはその教員名もセットで持っているので01に譲ったあとinneridを1ずつ下げてやる
+      # その他のときは単純にinneridを1ずつ下げてやるだけで良い
       if (delete_innerid % 100).zero?
         reviews.each do |r|
           if r.innerid == delete_innerid + 1
@@ -49,7 +55,8 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    makeinnerid = 0
+    # 科目に応じてinneridの大枠を決める
+    # 科目を指定しなかったときは無視するため0を設定する
     makeinnerid = case params[:review][:class]
                   when '数理科学基礎'
                     10_000
@@ -58,18 +65,22 @@ class ReviewsController < ApplicationController
                   else
                     0
                   end
-    unless makeinnerid == 0 then
+    unless makeinnerid.zero?
       reviews = Review.order(:innerid).all
+      # すでにコメントの付いている教員であれば，その教員の全コメントのうち最後尾に新しいコメントを追加する
+      # コメント付きの教員の場合，00はtitleとしてその教員名を持っているが01以降はtitleが空なのでinneridの上３桁で判別するしかない
       reviews.each do |r|
         if r.title == params[:review][:title] || (makeinnerid % 100 == 1 && makeinnerid / 100 == r.innerid / 100)
           makeinnerid = r.innerid + 1
         end
       end
+      # まだコメントの付いていない教員の場合は，inneridのbbの部分を新たに付与するためにbbの最後尾を調べてそれ+1を設定する
       if (makeinnerid % 10_000).zero?
         reviews.each do |r|
           makeinnerid = (r.innerid / 100 + 1) * 100 if r.innerid / 10_000 == makeinnerid / 10_000
         end
       end
+      # コメントの付いている教員の場合はtitleは空で良い
       params[:review][:title] = '' if makeinnerid % 100 != 0
       Review.create(title: params[:review][:title], content: params[:review][:content], innerid: makeinnerid)
     end
